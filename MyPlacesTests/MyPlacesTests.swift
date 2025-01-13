d40a045bb2d32cef11cc20db26f20906f29b8ecb
ssdhddq <ssdhddq@vk.com>
2025-01-13 14:18:22 +0700

    Initial Commit

---
+++ MyPlacesTests/MyPlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlacesTests.swift
+//  MyPlacesTests
+//
+//  Created by SSDHDDQ on 13.01.2025.
+//
+
+import Testing
+@testable import MyPlaces
+
+struct MyPlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
