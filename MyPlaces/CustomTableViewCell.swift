a0aa76d2b23c31a96d130ce7852b9553c3c4cd45
ssdhddq <ssdhddq@vk.com>
2025-01-15 14:58:13 +0700

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by SSDHDDQ on 15.01.2025.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
